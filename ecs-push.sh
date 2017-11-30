1d70527431dc0c84e991d38e07aa4dfa6d95cc54
Curtis Patrick <curtis.patrick84@gmail.com>
2017-11-29 20:19:12 -0800

    Delete ecs-push.sh

--- ecs-push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-$(aws ecr get-login --region us-west-2 --profile pp --no-include-email)
-docker build -t gh-hook .
-docker tag gh-hook:latest 441599757612.dkr.ecr.us-west-2.amazonaws.com/gh-hook:latest
-docker push 441599757612.dkr.ecr.us-west-2.amazonaws.com/gh-hook:latest
-aws ecs stop-task --profile pp --cluster pretty-pallets --region us-west-2 --task $(aws ecs list-tasks --profile pp --cluster pretty-pallets --region us-west-2 --family gh-hook | grep arn | sed 's/"//g' | awk '{print $1}') | grep 'desiredStatus\|taskDefinitionArn'
