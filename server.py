6bcc758dfa9be185331321a9ccc2deb2651f8bd3
cpatrick <curtis.patrick@nimblestorage.com>
2017-11-29 19:16:04 -0800

    updating server logic

--- server.py
+++ server.py
@@ -2,6 +2,28 @@
 import hug
 import logging
 import json
+import requests
+
+
+def createstatus(url, state, context=None, description=None, target=None):
+    data = {'state': state}
+
+    if context is not None:
+        data['context'] = context
+
+    if description is not None:
+        data['description'] = description
+
+    if target is not None:
+        data['target_url'] = target
+
+    requests.post(
+        url,
+        auth=(USER, TOKEN),
+        headers={'content-type': 'application/json'},
+        data=json.dumps(data),
+        verify=False
+    )
 
 
 @hug.post(versions=1)
@@ -11,8 +33,17 @@ def listen(request, body):
     if 'X-GITHUB-EVENT' not in request.headers:
         logging.info("GitHub event header not set - Ignoring")
         return
-    else:
-        logging.info(str(body))
+
+    if request.headers['X-GITHUB-EVENT'] == 'pull_request':
+        logging.debug("GitHub pull request event received")
+
+        if body['action'] == 'opened':
+            createstatus(
+                url=pr['statuses_url'],
+                state='pending',
+                description='Initializing',
+                context='gh-runner'
+            )
 
 
 @hug.not_found()
