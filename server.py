d6544cbcbcb530a43a4b5fd780c856363faab898
cpatrick <curtis.patrick@nimblestorage.com>
2017-11-30 15:24:55 -0800

    final touches (hopefully)

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from subprocess import Popen, PIPE
 
 USER = os.environ.get('GH_USER')
 TOKEN = os.environ.get('GH_TOKEN')
+LOG_URL = os.environ.get('LOG_URL')
 
 logging.getLogger("requests").setLevel(logging.WARNING)
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
@@ -35,6 +36,12 @@ def createstatus(url, state, context=None, description=None, target=None):
     )
 
 
+def cleanupdir(dir):
+    # cleanup dir
+    rm = Popen(['rm', '-rf', f'{dir}'], cwd='./', stdout=PIPE, stderr=PIPE)
+    (rm_status, error) = rm.communicate()
+
+
 def processpullrequest(pr):
     createstatus(
         url=pr['statuses_url'],
@@ -47,43 +54,51 @@ def processpullrequest(pr):
 
     gitcommand = f"/usr/bin/git clone -b {pr['head']['ref']} --depth=1 https://github.com/{pr['head']['repo']['full_name']} ./{temprepo}"
 
-    git_query = Popen(gitcommand.split(), cwd='./', stdout=PIPE, stderr=PIPE)
+    git_query = Popen(gitcommand.split(), cwd='./scratch', stdout=PIPE, stderr=PIPE)
     (git_status, error) = git_query.communicate()
 
     if git_query.poll() == 0:
+        log = open(f'./static/{temprepo}.txt', 'a')
         createstatus(
             url=pr['statuses_url'],
             state='pending',
             description='Installing dependencies',
             context='gh-hook'
         )
-        npm_install = Popen(['npm', 'install'], cwd=f'./{temprepo}', stdout=PIPE, stderr=PIPE)
+
+        npm_install = Popen(['/usr/bin/npm', 'install'], cwd=f'./scratch/{temprepo}', stdout=log, stderr=log)
         (npm_status, error) = npm_install.communicate()
 
-        if npm_status == 0:
+        if npm_install.poll() == 0:
             createstatus(
                 url=pr['statuses_url'],
                 state='pending',
                 description='Running tests',
-                context='gh-hook'
+                context='gh-hook',
+                target=f'{LOG_URL}/static/{temprepo}.txt'
             )
-            test = Popen(['npm', 'test'], cwd=f'./{temprepo}', stdout=PIPE, stderr=PIPE)
+
+            test = Popen(['/usr/bin/npm', 'test'], cwd=f'./scratch/{temprepo}', stdout=log, stderr=log)
             (test_status, error) = test.communicate()
 
-            if test_status == 0:
+            if test.poll() == 0:
                 createstatus(
                     url=pr['statuses_url'],
                     state='success',
                     description='All tests passed',
-                    context='gh-hook'
+                    context='gh-hook',
+                    target=f'{LOG_URL}/static/{temprepo}.txt'
                 )
+                cleanupdir(temprepo)
             else:
                 createstatus(
                     url=pr['statuses_url'],
                     state='failure',
                     description='One or more tests failed',
-                    context='gh-hook'
+                    context='gh-hook',
+                    target=f'{LOG_URL}/static/{temprepo}.txt'
                 )
+                cleanupdir(temprepo)
         else:
             createstatus(
                 url=pr['statuses_url'],
@@ -91,6 +106,8 @@ def processpullrequest(pr):
                 description='Failed to install dependencies!',
                 context='gh-hook'
             )
+            cleanupdir(temprepo)
+
     else:
         createstatus(
             url=pr['statuses_url'],
@@ -98,6 +115,8 @@ def processpullrequest(pr):
             description='Unable to clone git repo!',
             context='gh-hook'
         )
+
+    return
 
 
 @hug.post(versions=1)
@@ -113,14 +132,23 @@ def listen(request, body):
 
         event = json.loads(body['payload'])
 
-        if 'opened' in event['action'] || event['action'] == 'synchronize':
+        if 'opened' in event['action'] or event['action'] == 'synchronize':
             processpullrequest(event['pull_request'])
+            return
+        else:
+            return
+    else:
+        return
 
 
 @hug.not_found()
 def not_found_handler():
     return "Not Found"
 
+@hug.static('/static')
+def static_dirs():
+    """Returns static directory names to be served."""
+    return('./static',)
 
 if __name__ == "__main__":
     logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(funcName)s: %(message)s', level=logging.DEBUG)
