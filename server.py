074e65fdb4767add819b42c9e961e5ac6d2bae2d
cpatrick <curtis.patrick@nimblestorage.com>
2017-11-30 13:42:45 -0800

    Dockerfile updates

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import logging
 import json
 import requests
 import os
+import uuid
+from subprocess import Popen, PIPE
 
 USER = os.environ.get('GH_USER')
 TOKEN = os.environ.get('GH_TOKEN')
@@ -32,9 +34,70 @@ def createstatus(url, state, context=None, description=None, target=None):
         verify=False
     )
 
-    logging.debug(data)
-    logging.debug(response.status_code)
-    logging.debug(response.json())
+
+def processpullrequest(pr):
+    createstatus(
+        url=pr['statuses_url'],
+        state='pending',
+        description='Initializing',
+        context='gh-hook'
+    )
+
+    temprepo = uuid.uuid4()
+
+    gitcommand = f"/usr/bin/git clone -b {pr['head']['ref']} --depth=1 https://github.com/{pr['head']['repo']['full_name']} ./{temprepo}"
+
+    git_query = Popen(gitcommand.split(), cwd='./', stdout=PIPE, stderr=PIPE)
+    (git_status, error) = git_query.communicate()
+
+    if git_query.poll() == 0:
+        createstatus(
+            url=pr['statuses_url'],
+            state='pending',
+            description='Installing dependencies',
+            context='gh-hook'
+        )
+        npm_install = Popen(['npm', 'install'], cwd=f'./{temprepo}', stdout=PIPE, stderr=PIPE)
+        (npm_status, error) = npm_install.communicate()
+
+        if npm_status == 0:
+            createstatus(
+                url=pr['statuses_url'],
+                state='pending',
+                description='Running tests',
+                context='gh-hook'
+            )
+            test = Popen(['npm', 'test'], cwd=f'./{temprepo}', stdout=PIPE, stderr=PIPE)
+            (test_status, error) = test.communicate()
+
+            if test_status == 0:
+                createstatus(
+                    url=pr['statuses_url'],
+                    state='success',
+                    description='All tests passed',
+                    context='gh-hook'
+                )
+            else:
+                createstatus(
+                    url=pr['statuses_url'],
+                    state='failure',
+                    description='One or more tests failed',
+                    context='gh-hook'
+                )
+        else:
+            createstatus(
+                url=pr['statuses_url'],
+                state='failure',
+                description='Failed to install dependencies!',
+                context='gh-hook'
+            )
+    else:
+        createstatus(
+            url=pr['statuses_url'],
+            state='failure',
+            description='Unable to clone git repo!',
+            context='gh-hook'
+        )
 
 
 @hug.post(versions=1)
@@ -50,13 +113,8 @@ def listen(request, body):
 
         event = json.loads(body['payload'])
 
-        if 'opened' in event['action']:
-            createstatus(
-                url=event['pull_request']['statuses_url'],
-                state='pending',
-                description='Initializing',
-                context='gh-hook'
-            )
+        if 'opened' in event['action'] || event['action'] == 'synchronize':
+            processpullrequest(event['pull_request'])
 
 
 @hug.not_found()
